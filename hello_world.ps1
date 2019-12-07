b7891bf831b33420fcba22916ffc2a8dcaf2af30
Nick <nick.rettinghouse@gmail.com>
2019-12-06 21:06:19 -0800

    20191206_2106

--- hello_world.ps1
+++ hello_world.ps1
@@ -1,6 +1,8 @@
 Write-Host 'Hello, world!'
 Write-Host 'This is the second line.'
-$Directories = Get-ChildItem -Path C:\users\nickr\Desktop -Directory -Recurse
+#$Directories = Get-ChildItem -Path C:\users\nickr\Desktop -Directory -Recurse
+#$Directories = Get-ChildItem -Path ../ -Directory -Recurse
+$Directories = Get-ChildItem -Path ../ -Name -Recurse
 Write-Output -InputObject $Directories
 
 

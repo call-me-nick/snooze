89fde301f226d86a61ffc45be3f4ded1f9008659
Nick <nick.rettinghouse@gmail.com>
2019-12-31 11:11:17 -0800

    20191231_1111

--- hello_world.ps1
+++ hello_world.ps1
@@ -1,8 +1,16 @@
-Write-Host 'Hello, world!'
-Write-Host 'This is the second line.'
+#Write-Host 'Hello, world!'
+#Write-Host 'This is the second line.'
 #$Directories = Get-ChildItem -Path C:\users\nickr\Desktop -Directory -Recurse
 #$Directories = Get-ChildItem -Path ../ -Directory -Recurse
 $Directories = Get-ChildItem -Path ../ -Name -Recurse
-#Write-Output -InputObject $Directories | %{$_ -replace "^", "file://"}
-Write-Output -InputObject $Directories | %{$_ -replace "\\", "/"}
+#Write-Output -InputObject $Directories | `
+#    %{$_ -replace "\\", "/"} | `
+#    %{$_ -replace "^", "<a href=\"file://>"} |\
+foreach ($the_dir in $Directories){
+    the_link = $the_dir |`
+        %{$_ -replace "\\", "/"}|`
+        %{$_ -replace "^", "<a href=`"file://"}|`
+        %{$_ -replace "$", "`">link<a>"}
+    "$the_dir,$the_link"
+}
 

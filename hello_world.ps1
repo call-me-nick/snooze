029a5ccb7f38075cdfa27d9df147f73065c33b78
Nick <nick.rettinghouse@gmail.com>
2019-12-31 12:04:06 -0800

    20191231_1204

--- hello_world.ps1
+++ hello_world.ps1
@@ -1,7 +1,7 @@
 # Build a CSV from a list of directories.
 #  With hyperlinks.
 $Directories = Get-ChildItem -Path ./ -Name -Recurse
-$csv_lines = @()
+$csv_lines = @("Dir,LibreOffice_Hyperlink,HTML_Link,Simple_Link")
 foreach ($dir in $Directories){
     $dir = $dir | %{$_ -replace "\\", "/"}
     $html_link = $dir |`
@@ -9,9 +9,5 @@ foreach ($dir in $Directories){
         %{$_ -replace "$", "`">link<a>"}
     $csv_lines += "$dir,=HYPERLINK(`"$dir`"),$html_link,file://$dir"
 }
-Export-Csv `
-    -InputObject $csv_lines `
-    -Property Dir,LibreOffice_Hyperlink,HTML_Link,Simple_Link `
-    -Path ./my_links.csv
-"Wrote ./my_links.csv"
-"Use `"Import CSV`" in Excel"
+Export-Csv -InputObject $csv_lines -Path ./my_links.csv
+"Wrote ./my_links.csv -> Now `"Import CSV`" in Excel"

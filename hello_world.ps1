a59ebccc60b4b9f077729ccbb6dd063d71e72ceb
Nick <nick.rettinghouse@gmail.com>
2019-12-31 12:42:26 -0800

    20191231_1242

--- hello_world.ps1
+++ hello_world.ps1
@@ -1,13 +1,28 @@
 # Build a CSV from a list of directories.
 #  With hyperlinks.
+# Needs to handle:
+#   Excel hyperlinks
+#   Files with spaces in the name.
 $Directories = Get-ChildItem -Path ./ -Name -Recurse
-$csv_lines = @("Dir,LibreOffice_Hyperlink,HTML_Link,Simple_Link")
+$csv_lines = @() #@("Dir,LibreOffice_Hyperlink,HTML_Link,Simple_Link")
+#foreach ($dir in $Directories){
+#    $dir = $dir | %{$_ -replace "\\", "/"}
+#    $html_link = $dir |`
+#        %{$_ -replace "^", "<a href=`"file://"}|`
+#        %{$_ -replace "$", "`">link<a>"}
+#    $csv_lines += "$dir,=HYPERLINK(`"$dir`"),$html_link,file://$dir"
+#}
 foreach ($dir in $Directories){
     $dir = $dir | %{$_ -replace "\\", "/"}
     $html_link = $dir |`
         %{$_ -replace "^", "<a href=`"file://"}|`
         %{$_ -replace "$", "`">link<a>"}
-    $csv_lines += "$dir,=HYPERLINK(`"$dir`"),$html_link,file://$dir"
+    $csv_lines += [dir_object]@{
+        Dir = "$dir"
+        LibreOffice_Hyperlink = "=HYPERLINK(`"$dir`")"
+        HTML_Link = "$html_link"
+        SimpleLink = "file://$dir"
+    }
 }
-$csv_lines | Select-Object * | Export-Csv -Path ./my_links.csv -NoTypeInformation
+$csv_lines  Export-Csv -Path ./my_links.csv -NoTypeInformation
 "Wrote ./my_links.csv -> Now `"Import CSV`" in Excel"

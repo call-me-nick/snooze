4b309b2cc6449a6cd46c5e9f417cabae358dd8fc
Nick <nick.rettinghouse@gmail.com>
2019-12-31 13:11:05 -0800

    20191231_1311

--- hello_world.ps1
+++ hello_world.ps1
@@ -1,17 +1,11 @@
 # Build a CSV from a list of directories.
 #  With hyperlinks.
 # Needs to handle:
-#   Excel hyperlinks
+#   Excel hyperlinks.
 #   Files with spaces in the name.
 $Directories = Get-ChildItem -Path ./ -Name -Recurse
-$csv_lines = @() #@("Dir,LibreOffice_Hyperlink,HTML_Link,Simple_Link")
-#foreach ($dir in $Directories){
-#    $dir = $dir | %{$_ -replace "\\", "/"}
-#    $html_link = $dir |`
-#        %{$_ -replace "^", "<a href=`"file://"}|`
-#        %{$_ -replace "$", "`">link<a>"}
-#    $csv_lines += "$dir,=HYPERLINK(`"$dir`"),$html_link,file://$dir"
-#}
+$csv_lines = @()
+
 class dir_object
 {
     [ValidateNotNullOrEmpty()][string]$Dir
@@ -21,14 +15,13 @@ class dir_object
 }
 
 $Directories | ForEach-Object {
-    #$dir = $dir | %{$_ -replace "\\", "/"}
     $dir = %{$_ -replace "\\", "/"}
     $html_link = $dir |`
         %{$_ -replace "^", "<a href=`"file://"}|`
         %{$_ -replace "$", "`">link<a>"}
     $csv_lines += [dir_object]@{
         Dir = "$dir"
-        LibreOffice_Hyperlink = "=HYPERLINK(`"$dir`")"
+        LibreOffice_Hyperlink = "=HYPERLINK(`"$dir`", "Link")"
         HTML_Link = "$html_link"
         Simple_Link = "file://$dir"
     }
